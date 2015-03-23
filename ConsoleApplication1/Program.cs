e7d986b2f40c37c6ba1227a57bc0c7f5b8362190
Seth Johnson <sethjdesigns@gmail.com>
2015-03-23 11:21:07 -0400

    Print # of Args
    Print number of args in Program to the Console.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            
+            Console.WriteLine(args.Length);
+            Console.ReadLine();
         }
     }
 }
